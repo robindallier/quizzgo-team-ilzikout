55c14cf48f6b370cf360ec7322021125a0566579
Robin <imredyt85@gmail.com>
2025-11-13 11:43:10 +0100

    Implémentation complète des questions et réponses de cyber

--- cyber.go
+++ cyber.go
@@ -1,7 +1,18 @@
 package quiz
 
-CyberQ1 = "Quel type d’attaque consiste à submerger un serveur de requêtes pour le rendre indisponible ?"
-CyberR1 = "DDoS"
+const (
+	CyberQ1 = "Quel type d’attaque consiste à submerger un serveur de requêtes pour le rendre indisponible ?"
+	CyberR1 = "DDoS"
 
-CyberQ2 = "Quel est le rôle d’un pare-feu (firewall) ?"
-CyberR2 = "Filtrer le trafic réseau selon des règles"
+	CyberQ2 = "Quel est le rôle d’un pare-feu (firewall) ?"  
+	CyberR2 = "Filtrer le trafic réseau selon des règles"
+
+	CyberQ3 = ""
+	CyberR3 = ""
+
+	CyberQ4 = ""
+	CyberR4 = ""
+
+	CyberQ5 = ""
+	CyberR5 = ""
+)
