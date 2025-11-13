ab4453e0f1e9bedf8ad37527ca299988734f5636
Robin <imredyt85@gmail.com>
2025-11-13 11:29:16 +0100

    Implémentation de questions cybersécurité

--- cyber.go
+++ cyber.go
@@ -1,1 +1,7 @@
 package quiz
+
+CyberQ1 = "Quel type d’attaque consiste à submerger un serveur de requêtes pour le rendre indisponible ?"
+CyberR1 = "DDoS"
+
+CyberQ2 = "Quel est le rôle d’un pare-feu (firewall) ?"
+CyberR2 = "Filtrer le trafic réseau selon des règles"
